d387709fb25d992c7799726af3c2045e872a5035
Rohit-0508 <rohitranaut91@gmail.com>
2024-06-03 16:36:27 +0530

    first commit

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './DescriptionBox.css'
+export const DescriptionBox = () => {
+  return (
+    <div  className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Discover endless possibilities at SHOP. Shop curated collections of top-quality products, enjoy seamless browsing, secure transactions, and fast shipping. Your perfect find is just a click away.</p>
+            <p>Shop smarter, live better. SHOP offers a curated selection of top-quality products, easy browsing, and secure transactions. Elevate your shopping experience today</p>
+        </div>
+    </div>
+  )
+}
---
+++ frontend/src/Pages/Cart.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react'
+import { CartItem } from '../Components/CartItem/CartItem'
+import { ShopContext } from '../Context/ShopContext'
+
+export const Cart = () => {
+  
+
+  return (
+    <div >
+      <CartItem />
+    </div>
+  )
+}
